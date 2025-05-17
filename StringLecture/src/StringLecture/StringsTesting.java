88e8e3be2465cda7431eaec984904eb48e152dfb
Dayan Wishwanath <Dayan Wishwanath@MSI>
2025-05-17 18:56:30 +0530

    String Lecture

---
+++ StringLecture/src/StringLecture/StringsTesting.java
@@ -0,0 +1,51 @@
+package StringLecture;
+
+public class StringsTesting {
+
+	public static void main (String args[]) {
+		
+		
+//		char arry[] = {'S','L','I','I','T'};
+//		
+//		String s= new String(arry);
+//		
+//		System.out.println(arry);
+//		
+//		String d = new String("Dayan");
+//		System.out.println(d);
+//		
+//		String degree = "Information Technology";
+//		System.out.println(degree);
+//	
+		
+//		StringBuffer sb= new StringBuffer("Java StringBuffer Reverse Example");
+//		System.out.println("Original StringBuffer Content : " + sb);
+//		sb.reverse();
+//		System.out.println("Reversed StringBuffer Content : "+ sb);
+		
+//		String s1= "Java";
+//		String s2= "java";
+//		System.out.println(s1.equalsIgnoreCase(s2));
+//		String s3=new String("java");
+//		String s4=new String("Java");
+//		String s5=s4;
+//		
+//		System.out.println (s1 == s2);
+//		System.out.println (s1.equals( s2));
+//		System.out.println (s1.equals( "Java"));
+//		System.out.println (s3.equals( s4));
+//		System.out.println (s5.equals( s4));
+//		System.out.println (s5 == s4);
+		
+		String hw = "Hello".concat(" World"); // "Hello World"
+		
+		System.out.println(hw);
+		System.out.println(hw.toLowerCase());
+		System.out.println(hw);
+		System.out.println(hw.substring(0,7));
+		System.out.println(hw.replace("Hello", "Hi"));
+		System.out.println(hw.trim());
+	}
+	
+	
+}
